7689dc063f3a77ccacf893e1b03fdb31a2bb2ae9
Louise Aas Holm <louise.aas@sund.ku.dk>
2023-04-26 11:55:31 +0200

    Tidy data management and piping

--- R/learning.R
+++ R/learning.R
@@ -1,22 +1,52 @@
-# Basics of R -------------------------------------------------------------
-
-colnames(airquality)
-
-str(airquality)
-
-summary(airquality)
-
-1 + 1
-
-mean(2:6)
-
-
-# Loading packages --------------------------------------------------------
-
+# Load packages
 library(tidyverse)
-r3::check_git_config()
+library(NHANES)
 
-#Did I complete the exercise correctly?
+# Looking at data
+glimpse(NHANES)
 
-#This will be used for testing out Git
-#When a conflict happens
+# Selecting columns
+select(NHANES, Age)
+
+select(NHANES, Age, Weight, BMI)
+
+select(NHANES, -HeadCirc)
+
+select(NHANES, starts_with("BP"))
+
+select(NHANES, ends_with("Day"))
+
+select(NHANES, contains("Age"))
+
+# Create smaller NHANES dataset
+nhanes_small <- select(NHANES, Age, Gender, BMI, Diabetes, PhysActive, BPSysAve, BPDiaAve, Education)
+nhanes_small
+
+# Renaming columns
+nhanes_small <- rename_with(nhanes_small, snakecase::to_snake_case)
+
+# Renaming specific columns
+nhanes_small <- rename(nhanes_small, sex = gender)
+
+# Trying out the pipe
+colnames(nhanes_small)
+
+nhanes_small %>%
+  colnames()
+
+nhanes_small %>%
+  select(phys_active) %>%
+  rename(physically_active = phys_active)
+
+nhanes_small %>%
+  select(bp_sys_ave, education)
+
+nhanes_small %>%
+  rename(bp_sys = bp_sys_ave, bp_dia = bp_dia_ave)
+
+nhanes_small %>%
+  select(bmi, contains("age"))
+
+nhanes_small %>%
+  select(starts_with("bp_")) %>%
+  rename(bp_systolic = bp_sys_ave)
