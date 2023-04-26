9e11c16368ff6a5fc15c1c7fcfb3175112c96fdf
Louise Aas Holm <louise.aas@sund.ku.dk>
2023-04-26 13:21:27 +0200

    Logical condition

--- R/learning.R
+++ R/learning.R
@@ -50,3 +50,31 @@ nhanes_small %>%
 nhanes_small %>%
   select(starts_with("bp_")) %>%
   rename(bp_systolic = bp_sys_ave)
+
+# Filtering of data
+nhanes_small %>%
+  filter(phys_active != "No")
+
+nhanes_small %>%
+  filter(bmi >= 25)
+
+# Combining logical operators (logical conditions)
+nhanes_small %>%
+  filter(bmi >= 25 & phys_active == "No")
+
+nhanes_small %>%
+  filter(bmi >= 25 | phys_active == "No")
+
+# Arrange data
+nhanes_small %>%
+  arrange(desc(age))
+
+nhanes_small %>%
+  arrange(education, age)
+
+# Transform data
+nhanes_small %>%
+  mutate(age = age * 12, logged_bmi = log(bmi))
+
+nhanes_small %>%
+  mutate(old = if_else(age >= 30, "Yes", "No"))
